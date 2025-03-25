715661add5a016c05b132d72660d4e48b7cd67cf
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-03-25 19:34:18 +0600

    push to hosts in compose script

--- src/bin/hosts_push.ps1
+++ src/bin/hosts_push.ps1
@@ -5,7 +5,7 @@ param (
 
 if (-not (Select-String -Path $HOST_FILE_WINDOWS -Pattern ([regex]::Escape($HOST_ENTRY)))) {
     Start-Process powershell -Verb RunAs -Wait -PassThru -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', "Start-Sleep -Seconds 1; Add-Content -Path '$HOST_FILE_WINDOWS' -Value '`n$HOST_ENTRY'"
-    Write-Output 'Запись добавлена в файл hosts.'
+    Write-Output 'The entry has been added to the hosts file.'
 } else {
-    Write-Output 'Запись уже существует в файле hosts.'
+    Write-Output 'The entry already exists in the hosts file.'
 }
