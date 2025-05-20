47a47cd5c7256c288dff5e251a25acf4e28d5932
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-05-20 16:51:05 +0600

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,12 +6,12 @@ BINARY_NAME="docky"
 INSTALL_DIR="/usr/local/bin"
 REPO="BkycHblu-6oPwuK/compose"
 
-echo "Downloading $BINARY_NAME..."
+echo "Установка $BINARY_NAME..."
 curl https://raw.githubusercontent.com/BkycHblu-6oPwuK/compose/main/build/docky > "$INSTALL_DIR/$BINARY_NAME" || {
-    echo "Failed to download binary"
+    echo "Ошибка загрузки $BINARY_NAME"
     exit 1
 }
 
 chmod +x "$INSTALL_DIR/$BINARY_NAME"
-"$INSTALL_DIR/$BINARY_NAME" clean-cache
-echo "Successfully installed $BINARY_NAME to $INSTALL_DIR"
+echo "Установка прошла успешно"\n
+echo "Выполните команду $BINARY_NAME clean-cache для очистки кеша"
